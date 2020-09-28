9708220cd77c179162779c4267f5e5b6dc2424ef
YassinL <yassin.leclercq1@gmail.com>
2020-09-28 16:16:25 +0200

    modification de la config.json en config.js

---
+++ config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    username: 'root',
+    password: 'root',
+    database: 'ebooks_development',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+  test: {
+    username: 'root',
+    password: 'root',
+    database: 'ebooks_test',
+    host: '127.0.0.1',
+    dialect: ' mysql',
+  },
+  production: {
+    username: 'root',
+    password: 'root',
+    database: 'ebooks_production',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+};
